42f7843688226eddecea4c7ba4bc6ed546adc508
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-06 22:05:07 -0300

    06/11

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,10 +1,24 @@
 
 async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/TamirisCeolin/Ciencia-de-dados-Tamiris/refs/heads/main/api-main/numero-usuarios.json';
-    const res = await fetch(url);
-    const dados = await res.json();
+  const url = 'https://raw.githubusercontent.com/TamirisCeolin/Ciencia-de-dados-Tamiris/refs/heads/main/api-main/numero-usuarios.json';
+  const res = await fetch(url);
+  const dados = await res.json();
+  const nomeDasRedes = Object.keys(dados);
+  const quantidadeUsuarios = Object.values(dados);
 
-    console.log(dados);
-  }
-  
-  quantidadeUsuarios();
+  const data = [
+    {
+      x: nomeDasRedes,
+      y: quantidadeUsuarios,
+      type: 'bar'
+    }
+  ]
+
+  const grafico = document.createElement('div')
+  grafico.className = 'grafico'
+  document.getElementById('graficos-container').appendChild(grafico)
+  Plotly.newPlot(grafico, data)
+
+}
+
+quantidadeUsuarios();
