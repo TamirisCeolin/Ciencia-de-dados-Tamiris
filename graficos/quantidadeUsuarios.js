4e23acb3a16374aea46efa543e96658b017ce3d1
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-07 08:57:19 -0300

    07/11

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -18,10 +18,15 @@ async function quantidadeUsuarios() {
     }
   ]
 
+  const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color')
+}
+
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
+  Plotly.newPlot(grafico, data, layout)
 
 }
 
