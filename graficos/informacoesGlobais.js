f1dd48253fb28141034f085f26db90ec26bde718
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-11-19 16:29:49 -0300

    19/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -12,8 +12,8 @@ async function visualizarInformacoesGlobais() {
     
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo} bilhões </span> 
-    de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas} bilhões </span> estão conectadas 
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span> 
+    de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões </span> estão conectadas 
     em alguma rede social e passam em média <span>${horas} horas </span> e <span> ${minutos} 
     minutos </span> conectadas. <br>Isso significa que aproximadamente <span>${porcentagemConectada}% </span> 
     de pessoas estão conectadas em alguma rede social.`
